343ce61a73d2a9bdd7376fcbc0dc8661a9ecf34c
JonKadriu <jon.f.kadriu@gmail.com>
2024-07-16 10:07:31 +0200

    Add files via upload

---
+++ Sorting lists.py
@@ -0,0 +1,8 @@
+salary = [340, 210, 450, 600, 230, 500, 550]
+shumaepagave = 0
+for paga in salary:
+    shumaepagave = shumaepagave + paga
+print(f"Shuma e pagave eshte: {shumaepagave}")
+
+pagamesatare = shumaepagave / len(salary)
+print(f"Paga mesatare eshte: {pagamesatare}")
