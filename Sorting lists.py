37b49ddd89628bed0f26b29453346aaac5fdbe6d
JonKadriu <jon.f.kadriu@gmail.com>
2024-07-16 10:30:48 +0200

    Add files via upload

--- Sorting lists.py
+++ Sorting lists.py
@@ -1,8 +1,20 @@
 salary = [340, 210, 450, 600, 230, 500, 550]
-shumaepagave = 0
+shuma = 0
 for paga in salary:
-    shumaepagave = shumaepagave + paga
-print(f"Shuma e pagave eshte: {shumaepagave}")
+    shuma = shuma + paga
+print(f"Shuma e pagave eshte: {shuma}")
 
-pagamesatare = shumaepagave / len(salary)
+pagamesatare = shuma / len(salary)
 print(f"Paga mesatare eshte: {pagamesatare}")
+
+pagaMin = salary[0]
+for pagaAktuale in salary:
+    if(pagaAktuale < pagaMin):
+        pagaMin = pagaAktuale
+        print(f"Paga minimale eshte {pagaMin}")
+
+pagaMax = salary[0]
+for pagaAktuale in salary:
+    if(pagaAktuale > pagaMax):
+        pagaMax = pagaAktuale
+        print(f"Paga maksimale eshte {pagaMax}")
